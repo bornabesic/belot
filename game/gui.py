095a5075812a28ccb2153b1130496690ac221f72
Borna Bešić <besic.borna@gmail.com>
2018-09-30 17:53:38 +0200

    Refactor game/gui.py

--- game/gui.py
+++ game/gui.py
@@ -11,41 +11,41 @@ _screen_size = _screen_width, _screen_height = (800, 800)
 # Card sprites
 sprites = {
     # Herc
-    Card(Suit.HERC, Rank.VII): pygame.image.load("game/assets/cards/herc-7.png"),
-    Card(Suit.HERC, Rank.VIII): pygame.image.load("game/assets/cards/herc-8.png"),
-    Card(Suit.HERC, Rank.IX): pygame.image.load("game/assets/cards/herc-9.png"),
-    Card(Suit.HERC, Rank.X): pygame.image.load("game/assets/cards/herc-10.png"),
+    Card(Suit.HERC, Rank.VII):   pygame.image.load("game/assets/cards/herc-7.png"),
+    Card(Suit.HERC, Rank.VIII):  pygame.image.load("game/assets/cards/herc-8.png"),
+    Card(Suit.HERC, Rank.IX):    pygame.image.load("game/assets/cards/herc-9.png"),
+    Card(Suit.HERC, Rank.X):     pygame.image.load("game/assets/cards/herc-10.png"),
     Card(Suit.HERC, Rank.DECKO): pygame.image.load("game/assets/cards/herc-decko.png"),
-    Card(Suit.HERC, Rank.DAMA): pygame.image.load("game/assets/cards/herc-dama.png"),
+    Card(Suit.HERC, Rank.DAMA):  pygame.image.load("game/assets/cards/herc-dama.png"),
     Card(Suit.HERC, Rank.KRALJ): pygame.image.load("game/assets/cards/herc-kralj.png"),
-    Card(Suit.HERC, Rank.AS): pygame.image.load("game/assets/cards/herc-as.png"),
+    Card(Suit.HERC, Rank.AS):    pygame.image.load("game/assets/cards/herc-as.png"),
     # Karo
-    Card(Suit.KARO, Rank.VII): pygame.image.load("game/assets/cards/karo-7.png"),
-    Card(Suit.KARO, Rank.VIII): pygame.image.load("game/assets/cards/karo-8.png"),
-    Card(Suit.KARO, Rank.IX): pygame.image.load("game/assets/cards/karo-9.png"),
-    Card(Suit.KARO, Rank.X): pygame.image.load("game/assets/cards/karo-10.png"),
+    Card(Suit.KARO, Rank.VII):   pygame.image.load("game/assets/cards/karo-7.png"),
+    Card(Suit.KARO, Rank.VIII):  pygame.image.load("game/assets/cards/karo-8.png"),
+    Card(Suit.KARO, Rank.IX):    pygame.image.load("game/assets/cards/karo-9.png"),
+    Card(Suit.KARO, Rank.X):     pygame.image.load("game/assets/cards/karo-10.png"),
     Card(Suit.KARO, Rank.DECKO): pygame.image.load("game/assets/cards/karo-decko.png"),
-    Card(Suit.KARO, Rank.DAMA): pygame.image.load("game/assets/cards/karo-dama.png"),
+    Card(Suit.KARO, Rank.DAMA):  pygame.image.load("game/assets/cards/karo-dama.png"),
     Card(Suit.KARO, Rank.KRALJ): pygame.image.load("game/assets/cards/karo-kralj.png"),
-    Card(Suit.KARO, Rank.AS): pygame.image.load("game/assets/cards/karo-as.png"),
+    Card(Suit.KARO, Rank.AS):    pygame.image.load("game/assets/cards/karo-as.png"),
     # Pik
-    Card(Suit.PIK, Rank.VII): pygame.image.load("game/assets/cards/pik-7.png"),
-    Card(Suit.PIK, Rank.VIII): pygame.image.load("game/assets/cards/pik-8.png"),
-    Card(Suit.PIK, Rank.IX): pygame.image.load("game/assets/cards/pik-9.png"),
-    Card(Suit.PIK, Rank.X): pygame.image.load("game/assets/cards/pik-10.png"),
+    Card(Suit.PIK, Rank.VII):   pygame.image.load("game/assets/cards/pik-7.png"),
+    Card(Suit.PIK, Rank.VIII):  pygame.image.load("game/assets/cards/pik-8.png"),
+    Card(Suit.PIK, Rank.IX):    pygame.image.load("game/assets/cards/pik-9.png"),
+    Card(Suit.PIK, Rank.X):     pygame.image.load("game/assets/cards/pik-10.png"),
     Card(Suit.PIK, Rank.DECKO): pygame.image.load("game/assets/cards/pik-decko.png"),
-    Card(Suit.PIK, Rank.DAMA): pygame.image.load("game/assets/cards/pik-dama.png"),
+    Card(Suit.PIK, Rank.DAMA):  pygame.image.load("game/assets/cards/pik-dama.png"),
     Card(Suit.PIK, Rank.KRALJ): pygame.image.load("game/assets/cards/pik-kralj.png"),
-    Card(Suit.PIK, Rank.AS): pygame.image.load("game/assets/cards/pik-as.png"),
+    Card(Suit.PIK, Rank.AS):    pygame.image.load("game/assets/cards/pik-as.png"),
     # Tref
-    Card(Suit.TREF, Rank.VII): pygame.image.load("game/assets/cards/tref-7.png"),
-    Card(Suit.TREF, Rank.VIII): pygame.image.load("game/assets/cards/tref-8.png"),
-    Card(Suit.TREF, Rank.IX): pygame.image.load("game/assets/cards/tref-9.png"),
-    Card(Suit.TREF, Rank.X): pygame.image.load("game/assets/cards/tref-10.png"),
+    Card(Suit.TREF, Rank.VII):   pygame.image.load("game/assets/cards/tref-7.png"),
+    Card(Suit.TREF, Rank.VIII):  pygame.image.load("game/assets/cards/tref-8.png"),
+    Card(Suit.TREF, Rank.IX):    pygame.image.load("game/assets/cards/tref-9.png"),
+    Card(Suit.TREF, Rank.X):     pygame.image.load("game/assets/cards/tref-10.png"),
     Card(Suit.TREF, Rank.DECKO): pygame.image.load("game/assets/cards/tref-decko.png"),
-    Card(Suit.TREF, Rank.DAMA): pygame.image.load("game/assets/cards/tref-dama.png"),
+    Card(Suit.TREF, Rank.DAMA):  pygame.image.load("game/assets/cards/tref-dama.png"),
     Card(Suit.TREF, Rank.KRALJ): pygame.image.load("game/assets/cards/tref-kralj.png"),
-    Card(Suit.TREF, Rank.AS): pygame.image.load("game/assets/cards/tref-as.png")
+    Card(Suit.TREF, Rank.AS):    pygame.image.load("game/assets/cards/tref-as.png")
 }
 
 class GUI(Thread):
@@ -97,19 +97,19 @@ class GUI(Thread):
                 GUI.MessageType.SHOW_RIGHT,
                 GUI.MessageType.SHOW_UP
             ):
-                position, cardImage = item
+                cardImage = item[1]
                 image_width, image_height = cardImage.get_size()
 
-                if position is GUI.MessageType.SHOW_LEFT:
+                if messageType is GUI.MessageType.SHOW_LEFT:
                     x = _screen_width // 4 - image_width // 2
                     y = _screen_height // 2 - image_height // 2
-                elif position is GUI.MessageType.SHOW_DOWN:
+                elif messageType is GUI.MessageType.SHOW_DOWN:
                     x = _screen_width // 2 - image_width // 2
                     y = (_screen_height * 3) // 4 - image_height // 2
-                elif position is GUI.MessageType.SHOW_RIGHT:
+                elif messageType is GUI.MessageType.SHOW_RIGHT:
                     x = (_screen_width * 3) // 4 - image_width // 2
                     y = _screen_height // 2 - image_height // 2
-                elif position is GUI.MessageType.SHOW_UP:
+                elif messageType is GUI.MessageType.SHOW_UP:
                     x = _screen_width // 2 - image_width // 2
                     y = _screen_height // 4 - image_height // 2
 
